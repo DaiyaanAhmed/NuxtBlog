3d6b23c58c39c7a037df98dafe840b5b6e7cee24
DaiyaanAhmed <daaiyan.shaik@geospoc.com>
2020-11-16 18:54:54 +0530

    feat: Firebase added

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+import axios from "axios";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  asyncData(context) {
+    return axios
+      .get(
+        "https://nuxtblog-8a993.firebaseio.com/posts/" +
+          context.params.postId +
+          ".json"
+      )
+      .then(res => {
+        return {
+          loadedPost: { ...res.data, id: context.params.postId }
+        };
+      })
+      .catch(e => context.error());
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      this.$store.dispatch("editPost", editedPost).then(() => {
+        this.$router.push("/admin");
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Welcome to my Blog</h1>
+    </section>
+    <PostList :posts="loadedPosts" />
+  </div>
+</template>
+
+<script>
+import PostList from "@/components/Posts/PostList";
+
+export default {
+  components: {
+    PostList
+  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
+  }
+  // data() {
+  //   return {
+  //     loadedPosts: []
+  //   };
+  // },
+};
+</script>
+
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-image: url("~assets/images/bg.jpg");
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: lightblue;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
