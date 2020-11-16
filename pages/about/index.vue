d525f320e61cbb61f8b727761d3b1184a8121c31
DaiyaanAhmed <daaiyan.shaik@geospoc.com>
2020-11-16 19:02:10 +0530

    change

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="about-page">
-    <h1>This is my absolutely fantastic "About Me" Page!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-  </div>
+    <h1>About Me Page!</h1>
+    <p>About me About me About me About meAbout me About me About me About me About me About me About me About me About me </p>
+    <p>About me About me About me About meAbout me About me About me About me About me About me About me About me About me </p>
+    <p>About me About me About me About meAbout me About me About me About me About me About me About me About me About me </p>
 </template>
 
 <style scoped>
